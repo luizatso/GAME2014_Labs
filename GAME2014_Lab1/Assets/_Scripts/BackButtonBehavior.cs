1f6943cae509883a14c5b58aeb7e7bad44274bca
Luiza <117120748+luizatso@users.noreply.github.com>
2026-09-23 17:10:10 -0400

    Added next button with end scene and restart button with behaviors

--- GAME2014_Lab1/Assets/_Scripts/BackButtonBehavior.cs
+++ GAME2014_Lab1/Assets/_Scripts/BackButtonBehavior.cs
@@ -1,21 +1,11 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BackButtonBehavior : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void OnBackButtonPressed()
     {
         Debug.Log("Back Button Pressed");
+        SceneManager.LoadScene("Start");
     }
 }
--- GAME2014_Lab1/Assets/_Scripts/StartButtonBehavior.cs
+++ GAME2014_Lab1/Assets/_Scripts/StartButtonBehavior.cs
@@ -1,21 +1,11 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartButtonBehavior : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void OnStartButtonPressed()
     {
         Debug.Log("Start Button Pressed");
+        SceneManager.LoadScene("Play");
     }
 }
