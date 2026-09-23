f721e58d0f8f0c2a12aaa612b2a69ff26e0f15fc
Luiza <117120748+luizatso@users.noreply.github.com>
2026-09-23 16:56:12 -0400

    Added Play Scene with Play Scene label and Back Button

---
+++ GAME2014_Lab1/Assets/_Scripts/BackButtonBehavior.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class BackButtonBehavior : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnBackButtonPressed()
+    {
+        Debug.Log("Back Button Pressed");
+    }
+}
